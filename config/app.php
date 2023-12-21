0f6a33d03c5c44aef54d25e11b8ab29bbca2a98b
Ahmad Nurul Laiq <45028917+ahmadlaiq@users.noreply.github.com>
2023-12-21 13:25:53 +0700

    feat: laravel debugbar

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\Debugbar\ServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         /*
          * Application Service Providers...
@@ -184,6 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
